afa97c3fb28eb3c7a08e54050d0f7f1d91207e14
Tan Xi Ming <xmtan@paramit.com.my>
2021-03-29 13:26:00 +0800

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const custRouter = require("./api/customer/customer.router");
 const incomeRouter = require("./api/income/income.router");
 const expensesRouter = require("./api/expenses/expenses.router");
 const generalRouter = require("./api/general/general.router");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/customer", custRouter);
 app.use("/api/income", incomeRouter);
